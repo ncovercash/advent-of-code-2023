c37268c9e8b507c22312bf2b173158fdd72dc346
Noah Overcash <ncovercash@ua.edu>
2023-12-02 15:49:01 -0500

    Day 1

--- src/main/java/dev/ncovercash/Day1.java
+++ src/main/java/dev/ncovercash/Day1.java
@@ -1,6 +1,8 @@
 package dev.ncovercash;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
@@ -10,6 +12,95 @@ public class Day1 implements Solution {
   public String completeChallengePartOne(String filename) {
     List<String> lines = InputUtils.getLines(filename);
 
-    return "TODO";
+    return (
+      lines
+        .stream()
+        .map(line -> line.chars().filter(Character::isDigit).toArray())
+        .mapToInt(numbers ->
+          (numbers[0] - '0') * 10 + (numbers[numbers.length - 1] - '0')
+        )
+        .sum() +
+      ""
+    );
+  }
+
+  @Override
+  public String completeChallengePartTwo(String filename) {
+    List<String> lines = InputUtils.getLines(filename);
+
+    return (
+      lines
+        .stream()
+        .mapToInt(line -> {
+          log.info("{} -> {}, {}", line, getFirstNum(line), getLastNum(line));
+          return getFirstNum(line) * 10 + getLastNum(line);
+        })
+        .sum() +
+      ""
+    );
+  }
+
+  static Map<String, Integer> STRING_NUM_MAP = new HashMap<>();
+
+  static {
+    STRING_NUM_MAP.put("0", 0);
+    STRING_NUM_MAP.put("1", 1);
+    STRING_NUM_MAP.put("2", 2);
+    STRING_NUM_MAP.put("3", 3);
+    STRING_NUM_MAP.put("4", 4);
+    STRING_NUM_MAP.put("5", 5);
+    STRING_NUM_MAP.put("6", 6);
+    STRING_NUM_MAP.put("7", 7);
+    STRING_NUM_MAP.put("8", 8);
+    STRING_NUM_MAP.put("9", 9);
+
+    STRING_NUM_MAP.put("one", 1);
+    STRING_NUM_MAP.put("two", 2);
+    STRING_NUM_MAP.put("three", 3);
+    STRING_NUM_MAP.put("four", 4);
+    STRING_NUM_MAP.put("five", 5);
+    STRING_NUM_MAP.put("six", 6);
+    STRING_NUM_MAP.put("seven", 7);
+    STRING_NUM_MAP.put("eight", 8);
+    STRING_NUM_MAP.put("nine", 9);
+  }
+
+  private static int getFirstNum(String line) {
+    int bestIndex = Integer.MAX_VALUE;
+    int bestNum = -1;
+
+    for (Map.Entry<String, Integer> entry : STRING_NUM_MAP.entrySet()) {
+      // if the key is in the line
+      if (
+        line.indexOf(entry.getKey()) != -1 &&
+        line.indexOf(entry.getKey()) < bestIndex
+      ) {
+        bestIndex = line.indexOf(entry.getKey());
+        bestNum = entry.getValue();
+      }
+    }
+
+    log.info("Found first {} at [{}]", bestNum, bestIndex);
+
+    return bestNum;
+  }
+
+  private static int getLastNum(String line) {
+    int bestIndex = Integer.MIN_VALUE;
+    int bestNum = -1;
+
+    for (Map.Entry<String, Integer> entry : STRING_NUM_MAP.entrySet()) {
+      // if the key is in the line
+      if (
+        line.lastIndexOf(entry.getKey()) != -1 &&
+        line.lastIndexOf(entry.getKey()) > bestIndex
+      ) {
+        bestIndex = line.lastIndexOf(entry.getKey());
+        bestNum = entry.getValue();
+      }
+    }
+
+    log.info("Found last {} at [{}]", bestNum, bestIndex);
+    return bestNum;
   }
 }
--- src/test/java/dev/ncovercash/Day1Test.java
+++ src/test/java/dev/ncovercash/Day1Test.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import lombok.extern.log4j.Log4j2;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,8 +13,9 @@ class Day1Test {
 
   static List<Arguments> testCases() {
     return List.of(
-      Arguments.of("1/example.txt", "1412", ""),
-      Arguments.of("1/input.txt", "", "")
+      Arguments.of("1/example.txt", "142", "skip"),
+      Arguments.of("1/example2.txt", "skip", "281"),
+      Arguments.of("1/input.txt", "55172", "54925")
     );
   }
 
@@ -24,12 +24,16 @@ class Day1Test {
   void testAnswer(String filename, String expectedPart1, String expectedPart2) {
     Day1 instance = new Day1();
 
-    String actual = instance.completeChallengePartOne(filename);
-    log.info("Day 1 Part 1: {}", actual);
-    assertEquals(expectedPart1, actual);
+    if (!"skip".equals(expectedPart1)) {
+      String actual = instance.completeChallengePartOne(filename);
+      log.info("Day 1 Part 1: {}", actual);
+      assertEquals(expectedPart1, actual);
+    }
 
-    actual = instance.completeChallengePartTwo(filename);
-    log.info("Day 1 Part 2: {}", actual);
-    assertEquals(expectedPart2, actual);
+    if (!"skip".equals(expectedPart2)) {
+      String actual = instance.completeChallengePartTwo(filename);
+      log.info("Day 1 Part 2: {}", actual);
+      assertEquals(expectedPart2, actual);
+    }
   }
 }
