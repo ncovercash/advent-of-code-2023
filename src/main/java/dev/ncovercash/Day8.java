bd109cc2fef51bafcb7d530c84924bec9694c79f
Noah Overcash <noah@overca.sh>
2023-12-08 00:17:06 -0500

    8

---
+++ src/main/java/dev/ncovercash/Day8.java
@@ -0,0 +1,120 @@
+package dev.ncovercash;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.math3.util.ArithmeticUtils;
+
+@Log4j2
+public class Day8 implements Solution {
+
+  @Override
+  public String completeChallengePartOne(String filename) {
+    List<List<String>> puzzle = InputUtils.getLinesByClump(filename);
+
+    List<Character> instructions = puzzle
+      .get(0)
+      .get(0)
+      .chars()
+      .mapToObj(c -> (char) c)
+      .toList();
+
+    Map<String, Pair<String, String>> locations = new HashMap<>();
+
+    for (String node : puzzle.get(1)) {
+      String[] parts = node.split(" = ");
+      String src = parts[0];
+      parts[1] = parts[1].replace("(", "").replace(")", "");
+      String[] dest = parts[1].split(", ");
+
+      locations.put(src, Pair.of(dest[0], dest[1]));
+    }
+
+    String curLocation = "AAA";
+    int numSteps = 0;
+    for (int i = 0; true; i++) {
+      char direction = instructions.get(i % instructions.size());
+
+      if (direction == 'L') {
+        curLocation = locations.get(curLocation).getLeft();
+      } else if (direction == 'R') {
+        curLocation = locations.get(curLocation).getRight();
+      }
+
+      numSteps++;
+
+      if (curLocation.equals("ZZZ")) {
+        break;
+      }
+    }
+
+    return "" + numSteps;
+  }
+
+  @Override
+  public String completeChallengePartTwo(String filename) {
+    List<List<String>> puzzle = InputUtils.getLinesByClump(filename);
+
+    List<Character> instructions = puzzle
+      .get(0)
+      .get(0)
+      .chars()
+      .mapToObj(c -> (char) c)
+      .toList();
+
+    Map<String, Pair<String, String>> locations = new HashMap<>();
+
+    for (String node : puzzle.get(1)) {
+      String[] parts = node.split(" = ");
+      String src = parts[0];
+      parts[1] = parts[1].replace("(", "").replace(")", "");
+      String[] dest = parts[1].split(", ");
+
+      locations.put(src, Pair.of(dest[0], dest[1]));
+    }
+
+    List<String> curLocations = locations
+      .keySet()
+      .stream()
+      .filter(key -> key.endsWith("A"))
+      .collect(Collectors.toList());
+
+    List<Integer> steps = curLocations
+      .stream()
+      .map(l -> {
+        String curLocation = l;
+
+        int numSteps = 0;
+        for (int i = 0; true; i++) {
+          char direction = instructions.get(i % instructions.size());
+
+          if (direction == 'L') {
+            curLocation = locations.get(curLocation).getLeft();
+          } else if (direction == 'R') {
+            curLocation = locations.get(curLocation).getRight();
+          }
+
+          numSteps++;
+
+          if (curLocation.endsWith("Z")) {
+            break;
+          }
+        }
+
+        return numSteps;
+      })
+      .toList();
+
+    long lcm = 1;
+    for (int i = 0; i < steps.size(); i++) {
+      lcm = ArithmeticUtils.lcm(lcm, steps.get(i));
+    }
+
+    return "" + lcm;
+  }
+}
---
+++ src/test/java/dev/ncovercash/Day8Test.java
@@ -0,0 +1,40 @@
+package dev.ncovercash;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@Log4j2
+class Day8Test {
+
+  static List<Arguments> testCases() {
+    return List.of(
+      Arguments.of("8/example1a.txt", "2", "skip"),
+      Arguments.of("8/example1b.txt", "6", "skip"),
+      Arguments.of("8/example2.txt", "skip", "6"),
+      Arguments.of("8/input.txt", "17873", "15746133679061")
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("testCases")
+  void testAnswer(String filename, String expectedPart1, String expectedPart2) {
+    Day8 instance = new Day8();
+
+    if (!"skip".equals(expectedPart1)) {
+      String actual = instance.completeChallengePartOne(filename);
+      log.info("Part 1: {}", actual);
+      assertEquals(expectedPart1, actual);
+    }
+
+    if (!"skip".equals(expectedPart2)) {
+      String actual = instance.completeChallengePartTwo(filename);
+      log.info("Part 2: {}", actual);
+      assertEquals(expectedPart2, actual);
+    }
+  }
+}
